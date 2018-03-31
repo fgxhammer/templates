77f76dab432c1d3ab01f33e164d072e920325228
Franz Gollhammer <franz.gollhammer@gmail.com>
2018-03-31 18:41:06 +0200

    fixed sass sync, added autoprefixer

--- websites/gulpfile.js
+++ websites/gulpfile.js
@@ -1,23 +1,32 @@
-let gulp = require('gulp')
-let sass = require('gulp-sass')
-let browserSync = require('browser-sync').create() 
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const browserSync = require('browser-sync').create() 
 
-gulp.task('sass', () => {
-  return gulp.src('./src/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'))
+gulp.task('sass', () => 
+    gulp.src('sass/main.sass')
+    .pipe(sass({
+      includePaths:['scss','sass'],
+      onError: browserSync.notify
+    }))
+    .on('error', sass.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+      }))
+    .pipe(gulp.dest('css/'))
     .pipe(browserSync.stream())
-})
+)
 
 gulp.task('sync', () => {
   browserSync.init({
     server: './',
     notify: false
   })
-  gulp.watch('./src/sass/*.scss', ['sass'])
-  gulp.watch('./*.html').on('change', browserSync.reload)
-  gulp.watch('./src/pages/*.html').on('change', browserSync.reload)
-  gulp.watch('./src/js/*.js').on('change', browserSync.reload) 
+  gulp.watch(['sass/**'], ['sass'])
+  gulp.watch('*.html').on('change', browserSync.reload)
+  gulp.watch('pages/*.html').on('change', browserSync.reload)
+  gulp.watch('js/*.js').on('change', browserSync.reload) 
 })
 
 gulp.task('default', ['sync'])
