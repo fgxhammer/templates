d6135562f7204321cc609679e2156248a45e5a59
Franz Gollhammer <franz.gollhammer@gmail.com>
2018-04-02 15:58:15 +0200

    some changes

--- website-templates/gulp/gulp_sass_autoprefix_browser_sync/gulpfile.js
+++ website-templates/gulp/gulp_sass_autoprefix_browser_sync/gulpfile.js
@@ -1,22 +1,29 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
+const minifycss = require('gulp-minify-css')
+const rename = require('gulp-rename')
+const util = require('gulp-util')
+const log = util.log
 const browserSync = require('browser-sync').create() 
 
-gulp.task('sass', () => 
-    gulp.src('sass/main.sass')
+gulp.task('sass', () => {
+    log("Generate CSS files " + (new Date()).toString())
+    gulp.src('sass/main.scss')
     .pipe(sass({
       includePaths:['scss','sass'],
       onError: browserSync.notify
     }))
     .on('error', sass.logError)
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
+      browsers: ['last 3 versions']
       }))
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
     .pipe(browserSync.stream())
-)
+})
 
 gulp.task('sync', () => {
   browserSync.init({
